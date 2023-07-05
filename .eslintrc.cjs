e2628b96c326e75261b0b80e3191fc814782a375
nerdchanii <nerdchanii@gmail.com>
2023-07-05 18:16:35 +0900

    feat: Set up ESLint for Prettier and Typescript(#4)
    - Improve import consistency
    - Add ESLint configuration for TypeScript
    - Add ESLint configuration for Prettier
    
    Closes #4

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,53 @@
 module.exports = {
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, node: true },
   extends: [
     'eslint:recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'prettier', // must be last
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
+  plugins: ['@typescript-eslint', 'react-refresh', 'import'],
   rules: {
+    'import/no-unresolved': 'error',
     'react-refresh/only-export-components': 'warn',
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          ['parent', 'sibling', 'index'],
+          'object',
+          'type',
+        ],
+        pathGroups: [
+          {
+            pattern: '{react, react-dom}',
+            group: 'external',
+            position: 'before',
+          },
+          {
+            pattern: '*.(css|scss)',
+            group: 'index',
+            patternOptions: { matchBase: true },
+            position: 'after',
+          },
+        ],
+        alphabetize: { order: 'asc', caseInsensitive: true },
+        warnOnUnassignedImports: true,
+        'newlines-between': 'always',
+      },
+    ],
+  },
+  root: true,
+  settings: {
+    'import/resolver': {
+      typescript: {},
+    },
   },
 };
