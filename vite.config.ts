eed83f2b2cfee6d7bfe92479d0d626d97767fec6
nerdchanii <nerdchanii@gmail.com>
2023-07-10 20:10:09 +0900

    chore(setup): setup chrome extension development envrionment(#15)

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
+import { crx, defineManifest } from '@crxjs/vite-plugin';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
+import manifest from './manifest.json';
+
+// Define the manifest
+const manifestConfig = defineManifest({
+  ...manifest,
+  content_scripts: [
+    {
+      matches: ['https://github.com/*'],
+      js: ['./src/content/index.tsx'],
+    },
+  ],
+  background: {
+    service_worker: './src/background/index.ts',
+    type: 'module',
+  },
+});
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), crx({ manifest: manifestConfig })],
 });
