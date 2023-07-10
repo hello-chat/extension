c61ac2d0ad7ba0c088a383f3b2263df5b0c369e6
nerdchanii <nerdchanii@gmail.com>
2023-07-10 11:22:16 +0900

    feat: add husky for lint-staged

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 
-import viteLogo from '/vite.svg'; 
+// eslint-disable-next-line import/no-unresolved
+import viteLogo from '/vite.svg';
 
 import './App.css';
 import reactLogo from './assets/react.svg';
