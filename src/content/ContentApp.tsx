a7f0bf471acd1bb1cdec510601d85748a15715b9
nerdchanii <nerdchanii@gmail.com>
2023-07-11 15:06:52 +0900

    feat(ContentsScript): implement contentsScript modal (#18)

--- src/content/ContentApp.tsx
+++ src/content/ContentApp.tsx
@@ -1,5 +1,14 @@
-const ContentApp = () => {
-  return <div>ContentApp</div>;
+type ContentAppProps = {
+  onClose: () => void;
+};
+
+const ContentApp = ({ onClose }: ContentAppProps) => {
+  return (
+    <div>
+      <button onClick={onClose}>close</button>
+      <div>Hello-Chat-app</div>
+    </div>
+  );
 };
 
 export default ContentApp;
