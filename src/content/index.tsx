60170f742af7c98469e6dbf53e898aba22a8b4ff
nerdchanii <nerdchanii@gmail.com>
2023-07-11 15:06:52 +0900

    feat(ContentsScript): implement contentsScript modal (#18)

--- src/content/index.tsx
+++ src/content/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 
 import ContentApp from './ContentApp';
+import './index.css';
+import Modal from '../components/Modal';
 
 const root = document.createElement('div');
 document.body.appendChild(root);
@@ -14,7 +16,18 @@ const isBlack = BLACK_LISTS.some(
 if (!isBlack) {
   createRoot(root).render(
     <React.StrictMode>
-      <ContentApp />
+      <Modal
+        style={{
+          position: 'fixed',
+          left: '2rem',
+          bottom: '2rem',
+        }}
+      >
+        <Modal.Contents Component={ContentApp} />
+        <Modal.Trigger
+          Component={({ onOpen }) => <button onClick={onOpen}>open</button>}
+        />
+      </Modal>
     </React.StrictMode>
   );
 }
